6752ba31e3f3d1f3e180f4d1c7cf374c5a3c3921
Romainns <romainsintas@gmail.com>
2025-05-15 15:47:46 +0200

    Ajout de l'historique

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let calculer = document.getElementById('calculate');
+let historique = [];
+let compteur = 1;
 
 calculer.addEventListener('click', function() {
     let number1 = document.getElementById('number1');
     let number2 = document.getElementById('number2');
     let operation = document.getElementById('operation');
     let resultat = document.querySelector('.result');
+    let historyDiv = document.getElementById('history');
 
     let num1 = parseFloat(number1.value);
     let num2 = parseFloat(number2.value);
@@ -24,4 +27,12 @@ calculer.addEventListener('click', function() {
     }
 
     resultat.textContent = 'Le résultat est : ' + result;
-})
+
+    let operationText = `N°${compteur} : ${num1} ${operation.value} ${num2} = ${result}`;
+    historique.push(operationText);
+    compteur++;
+
+    let p = document.createElement('p');
+    p.textContent = operationText;
+    historyDiv.appendChild(p);
+});
